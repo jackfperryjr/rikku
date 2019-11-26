dc78360638a2d63c39af1a169e311a55a3c4edea
jackfperryjr <jack.franklin.perryjr@gmail.com>
2019-11-26 13:30:43 -0500

    Additional bug fix.

--- wwwroot/js/ready.js
+++ wwwroot/js/ready.js
@@ -18,6 +18,12 @@ $(document).ready(function() { // Activating icon related to navigated screen up
     (function mailChecker(){ // Self executing function that runs every 3 seconds.
         getMessageCount(); // Quick function to check for new messages.
         clearInterval(mailChecker);
+        if ($("#mail-page").is(":hidden")) { // If in the mailbox, get the new messages.
+            clearInterval(getMailbox);
+        }
+        if ($("#chat-page").is(":hidden")) { // If in the chat, get the new messages.
+            clearInterval(getChat);
+        }
         setTimeout(mailChecker, 3000)
     })();
 });
